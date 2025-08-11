06700c0f3ba0a400191b4c4b18f509e4a2c172f1
Xhoi-Muca <mucaxhoi3@gmail.com>
2025-08-11 09:34:51 +0200

    bug fix

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,8 +4,6 @@ import { Canvas } from '@react-three/fiber';
 import { useMediaQuery } from 'react-responsive';
 import { PerspectiveCamera } from '@react-three/drei';
 
-import Cube from '../components/Cube.jsx';
-import { Rings }  from '../components/Rings.jsx';
 import ReactLogo from '../components/ReactLogo.jsx';
 import Button from '../components/Button.jsx';
 import Target from '../components/Target.jsx';
